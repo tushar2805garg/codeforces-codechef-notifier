7384f8ccbcaafd98159ebde090eff8cf9dd79e15
tushar2805garg <gargtushar460@gmail.com>
2022-02-07 19:26:05 +0530

    Fixed minor bug in Codechef Script File

--- codechef-script.js
+++ codechef-script.js
@@ -3,14 +3,29 @@ if(document.querySelector("#edit-submit") === null || document.querySelector("#e
     console.log("A");
     if(document.querySelector(" span.right > a") !== null){
     console.log("B");
-        codechef_username=document.querySelector(" span.right > a").innerText;
+        // codechef_username=document.querySelector(" span.right > a").innerText;
+        codechef_username=document.querySelectorAll("span.right > a >span");
+        codechef_username=codechef_username[codechef_username.length-1].innerText;
 }
 }
 if(document.querySelector("body > header > div > div.l-header__user-block > div > div.l-username > div.l-dropdown-container--username > span")!==null){
     console.log("C");
-    codechef_username=document.querySelector("body > header > div > div.l-header__user-block > div > div.l-username > div.l-dropdown-container--username > span").innerText;
-}
+    // codechef_username=document.querySelector("body > header > div > div.l-header__user-block > div > div.l-username > div.l-dropdown-container--username > span").innerText;
+    codechef_username=document.querySelectorAll("body > header > div > div.l-header__user-block > div > div.l-username > div.l-dropdown-container--username > span");
+    codechef_username=codechef_username[codechef_username.length-1].innerText;
+  }
 if(codechef_username !== undefined){
+  // var isstar=false;
+  // for(var j=0;j<codechef_username.length;j++){
+  //   if(codechef_username[j]=='*') {
+  //     isstar=true;
+  //     break;
+  //   }
+  // }
+  // if(isstar==true){
+  //   var len=codechef_username.length;
+  //   codechef_username=codechef_username.substr(2,len-2);
+  // }
 chrome.storage.sync.set({"codechef_username": codechef_username}, function() {
     // console.log('Value is set to ' + value);
   });
